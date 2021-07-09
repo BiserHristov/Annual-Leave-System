efcd9448f55b26ffc67efadf6e61404c19a89e02
Biser Hristov <bhristov83@gmail.com>
2021-07-09 17:25:29 +0300

    Clean the project

---
+++ AnnualLeaveSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AnnualLeaveSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
